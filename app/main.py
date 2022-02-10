a2de013a446d43faff32762400017f2e5fb0028e
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-02-10 22:11:15 +0900

    update: Change trigger settings of scheduler

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-from apscheduler.triggers.interval import IntervalTrigger
+from apscheduler.triggers.cron import CronTrigger
 
 from bot import bot_job
 
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler(timezone='Asia/Tokyo')
-    scheduler.add_job(bot_job, IntervalTrigger(minutes=60))
+    scheduler.add_job(bot_job, CronTrigger(minutes=30))
     scheduler.start()
