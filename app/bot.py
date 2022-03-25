debf62adfb0026286a89770362a19d0cd773919b
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2022-03-26 04:31:54 +0900

    refactor: Create class for scheduler job

--- app/bot.py
+++ app/bot.py
@@ -8,102 +8,101 @@ from api import TwitterAPI
 from db import BotDBQuery
 
 
-TWEETS_FILE_PATH = 'app/data/tweets.tsv'
+class BotJob():
 
+    def __init__(self, tweets_file_path):
+        self.tweet_list = self._read_tweets(tweets_file_path)
 
-# 現在の日付と時刻を返す
-def datetime_now():
-    return datetime.now(
-        timezone(timedelta(hours=9))).isoformat(timespec='seconds')
-
-
-# ログの出力
-def output_log(text):
-    print('{} {}\n'.format(datetime_now(), text))
-
-
-# ツイートのデータの読み込み
-def read_tweets():
-    with open(TWEETS_FILE_PATH, encoding='utf-8', newline='') as f:
-        reader = csv.reader(f, delimiter='\t')
-        header = next(reader)
-
-        tweet_list = []
-        for row in reader:
-            tweet = {'images': []}
-            for key, value in zip(header, row):
-                if key == 'text':
-                    tweet[key] = value.replace('\\n', '\n')
-                elif key in ['img1', 'img2', 'img3', 'img4']:
-                    if value:
-                        tweet['images'].append(value)
-                else:
-                    tweet[key] = value
-            tweet_list.append(tweet)
-
-    return tweet_list
-
-
-# 未ツイートのツイートのインデクスを返す
-def not_tweeted_indices(tweet_list, tweeted_id_list):
-    return [i for i, tweet in enumerate(tweet_list)
-            if tweet['id'] not in tweeted_id_list]
-
-
-# tweeted_data.jsonのデータを作る
-def make_tweeted_data(tweet_list, tweeted_id_list):
-    return {
-        'last_update': datetime_now(),
-        'total': len(tweet_list),
-        'tweeted': len(tweeted_id_list),
-        'tweeted_id_list': tweeted_id_list,
-    }
-
-
-# ジョブ
-def bot_job():
-    twitter_api = TwitterAPI(config.TWITTER_API_KEY,
-                             config.TWITTER_API_KEY_SECRET,
-                             config.TWITTER_ACCESS_TOKEN,
-                             config.TWITTER_ACCESS_TOKEN_SECRET)
-    bot_db = BotDBQuery(config.DATABASE_URL,
-                        config.LOCAL_DB_USER,
-                        config.LOCAL_DB_PASS)
-
-    # テーブルの確認
-    bot_db.init_table()
-
-    while True:
+    def _read_tweets(self, tweets_file_path):
         # ツイートのデータの読み込み
-        tweet_list = read_tweets()
-        tweeted_id_list = bot_db.get_data()['tweeted_id_list']
+        with open(tweets_file_path, encoding='utf-8', newline='') as f:
+            reader = csv.reader(f, delimiter='\t')
+            header = next(reader)
 
-        # ツイート候補のインデクス
-        next_indices = not_tweeted_indices(tweet_list, tweeted_id_list)
+            tweet_list = []
+            for row in reader:
+                tweet = {'images': []}
+                for key, value in zip(header, row):
+                    if key == 'text':
+                        tweet[key] = value.replace('\\n', '\n')
+                    elif key in ['img1', 'img2', 'img3', 'img4']:
+                        if value:
+                            tweet['images'].append(value)
+                    else:
+                        tweet[key] = value
+                tweet_list.append(tweet)
 
-        # もし候補が無ければ（一巡）データを初期化し再読み込み
-        if next_indices:
-            break
-        else:
-            output_log('tweets have come full circle')
-            bot_db.update_data(make_tweeted_data(tweet_list, []))
+        return tweet_list
 
-    while True:
-        # ツイート候補を無作為に取り出しツイート
-        index = random.choice(next_indices)
-        is_success, api_code = twitter_api.post_tweet(tweet_list[index])
+    def _datetime_now(self):
+        # 現在の日付と時刻を返す
+        return datetime.now(
+            timezone(timedelta(hours=9))).isoformat(timespec='seconds')
 
-        # ツイートに成功したらツイート済みIDリストを更新しダンプ
-        # ツイートに失敗したら10秒待ってやりなおし
-        if is_success:
-            tweeted_id_list.append(tweet_list[index]['id'])
-            bot_db.update_data(make_tweeted_data(tweet_list, tweeted_id_list))
-            break
-        else:
-            if api_code == 187:  # 連続ツイートの拒否
-                output_log('[ERROR] Twitter API: code {} - status is a duplicate'.format(api_code))
-            elif api_code == 186:  # ツイート文字数オーバー
-                output_log('[ERROR] Twitter API: code {} - tweet needs to be a bit shorter'.format(api_code))
+    def _output_log(self, text):
+        # ログの出力
+        print('{} {}\n'.format(self._datetime_now(), text))
+
+    def _not_tweeted_indices(self, tweeted_id_list):
+        # 未ツイートのツイートのインデクスを返す
+        return [i for i, tweet in enumerate(self.tweet_list)
+                if tweet['id'] not in tweeted_id_list]
+
+    def _make_tweeted_data(self, tweeted_id_list):
+        # tweeted_data.jsonのデータを作る
+        return {
+            'last_update': self._datetime_now(),
+            'total': len(self.tweet_list),
+            'tweeted': len(tweeted_id_list),
+            'tweeted_id_list': tweeted_id_list,
+        }
+
+    def bot_job(self):
+        # 定期ジョブ
+        twitter_api = TwitterAPI(config.TWITTER_API_KEY,
+                                config.TWITTER_API_KEY_SECRET,
+                                config.TWITTER_ACCESS_TOKEN,
+                                config.TWITTER_ACCESS_TOKEN_SECRET)
+        bot_db = BotDBQuery(config.DATABASE_URL,
+                            config.LOCAL_DB_USER,
+                            config.LOCAL_DB_PASS)
+
+        # テーブルの確認
+        bot_db.init_table()
+
+        while True:
+            # ツイート済みのID
+            tweeted_id_list = bot_db.get_data()['tweeted_id_list']
+
+            # ツイート候補のインデクス
+            next_indices = self._not_tweeted_indices(tweeted_id_list)
+
+            # もし候補が無ければ（一巡）データを初期化し再読み込み
+            if next_indices:
+                break
             else:
-                output_log('[ERROR] Twitter API: code {}'.format(api_code))
-            time.sleep(10)
+                self._output_log('tweets have come full circle')
+                bot_db.update_data(
+                    self._make_tweeted_data([]))
+
+        while True:
+            # ツイート候補を無作為に取り出しツイート
+            index = random.choice(next_indices)
+            is_success, api_code = twitter_api.post_tweet(
+                self.tweet_list[index])
+
+            # ツイートに成功したらツイート済みIDリストを更新
+            # ツイートに失敗したら10秒待ってやりなおし
+            if is_success:
+                tweeted_id_list.append(self.tweet_list[index]['id'])
+                bot_db.update_data(
+                    self._make_tweeted_data(tweeted_id_list))
+                break
+            else:
+                if api_code == 187:  # 連続ツイートの拒否
+                    self._output_log('[ERROR] Twitter API: code {} - status is a duplicate'.format(api_code))
+                elif api_code == 186:  # ツイート文字数オーバー
+                    self._output_log('[ERROR] Twitter API: code {} - tweet needs to be a bit shorter'.format(api_code))
+                else:
+                    self._output_log('[ERROR] Twitter API: code {}'.format(api_code))
+                time.sleep(10)
--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 
-from bot import bot_job
+from bot import BotJob
+
+
+TWEETS_FILE_PATH = 'app/data/tweets.tsv'
 
 
 if __name__ == '__main__':
+    job = BotJob(TWEETS_FILE_PATH)
+
     scheduler = BlockingScheduler(timezone='Asia/Tokyo')
-    scheduler.add_job(bot_job, CronTrigger(minute=30))
+    scheduler.add_job(job.bot_job, CronTrigger(minute=30))
     scheduler.start()
