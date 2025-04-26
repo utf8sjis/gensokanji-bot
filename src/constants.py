f7a8a51279fa0b694958b8e68ea925e43413c22d
utf8sjis <89941856+utf8sjis@users.noreply.github.com>
2025-04-27 00:44:39 +0900

    Use schedule

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,5 @@
 import os
 from pathlib import Path
 
-BOT_JOB_ID = "bot_job"
-BOT_JOB_SCHEDULE = {"minute": 30}
-
 BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATA_DIR = BASE_DIR / "data"
